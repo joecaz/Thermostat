0ae353bb2d9e8a39ad62285695f77752ccc71e56
Joseph Cicinelli <jcicinelli@gmail.com>
2017-06-04 07:48:50 -0700

    Initial Commit

---
+++ Thermostat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Thermostat
+//
+//  Created by joecaz on 6/4/17.
+//  Copyright © 2017 Coyote Creek Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Thermostat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Thermostat
+//
+//  Created by joecaz on 6/4/17.
+//  Copyright © 2017 Coyote Creek Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
